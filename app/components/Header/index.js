a33f716037fcf344be6e7ca0d39a73518ca28d7e
Matteo Corazzin <matteo@bimah.co.uk>
2018-03-12 11:46:35 +0000

    Added main navigation

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -1,5 +1,17 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-const Header = () => (<header>This is the Header</header>);
+import styles from './header.scss';
+
+const Header = () => (
+  <header>
+    <div className={styles['main-navigation']}>
+      <ul>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/about">About</NavLink></li>
+      </ul>
+    </div>
+  </header>
+);
 
 export default Header;
